352094966f5d4131b74a29ab3312d28083eccc23
Mafe-SL <fernanda.mariastb@gmail.com>
2024-02-02 15:50:18 -0600

    Form v5

---
+++ src/components/FormAbout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function FormAbout() {
+  return (
+    <div className='text-gray-600'>
+        <form action="">
+            <label htmlFor="">Nombre</label>
+            <input type="text" name="" id="" />
+            <label htmlFor="">Apellidos</label>
+            <input type="text" name="" id="" />
+
+        </form>
+    </div>
+  )
+}
+
+export default FormAbout
