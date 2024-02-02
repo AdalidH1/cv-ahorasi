6d960d92dd28923d51bdd78102e52de58a766734
Adalidh1 <adalidh141@gmail.com>
2024-02-02 15:52:11 -0600

    v7

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,19 @@
 "use client"
+import axios from 'axios'
 import { SessionProvider, useSession } from 'next-auth/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Dashboard() {
   const {data:session} = useSession()
   console.log(session)
+  const [datos, setDatos] = useState([]);
+  useEffect(() => {
+    // Realiza la solicitud GET a la ruta de la API
+    axios.get(`/api/hobbies/1`).then((response) => {
+      setDatos(response.data);
+    });
+  }, []);
+      console.log(datos)
   return (
     <div>
       <h1 className=' font-bold text-black'>hola {session?.user.name}</h1>
