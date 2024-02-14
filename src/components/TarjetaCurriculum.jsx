6b2b16e3f0042d3263dcc4104ebac09e88a1a445
Mafe-SL <fernanda.mariastb@gmail.com>
2024-02-14 13:48:47 -0600

    Estilos actualizados para barra de busqueda

--- src/components/TarjetaCurriculum.jsx
+++ src/components/TarjetaCurriculum.jsx
@@ -66,12 +66,38 @@ const TarjetaCurriculum = () => {
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Buscar por nombre..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+      <div className="flex justify-center">
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-slate-500 dark:text-slate-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+
+          <input
+            type="text"
+            placeholder="Buscar por nombre..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="block w-full md:w-80 p-4 pl-10 text-sm text-slate-900 border-b-2 border-slate-200 rounded-lg bg-white-100 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+      </div>
+
+
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {filteredData.map((item) => (
           <div
